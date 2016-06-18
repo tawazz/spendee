c1f43f4e3a188d83037879b8d85f586bfb6cb59e
tawanda <tawanda.nyakudjga@gmail.com>
2016-06-18 11:01:52 +0800

    font changes

--- app/views/auth/login.php
+++ app/views/auth/login.php
@@ -2,11 +2,11 @@
 
 {% block content %}
 <div class="row" style="margin-top:10px;">
-    <div class="col-xs-12 col-sm-6 col-sm-offset-3">
+    <div class="col-xs-12 col-sm-8 col-sm-offset-2">
         <div class="panel panel-default">
             <div class="panel-body">
                 <div class="text-center img-responsive"><img src="{{baseUrl}}/images/icon.png" height="100" alt="logo" /></div>
-                <div class="page-header text-center"><h3>Spendee</h3></div>
+                <div class="page-header text-center"><h3 class="brand-font">Spendee</h3></div>
                 <form role="form" action="{{urlFor('post.login')}}" method="post" name="login" onsubmit="return checkLogin()">
                   <div class="form-group{%if errors.username%} has-error{%endif%}">
                     <label>Username</label>
@@ -22,10 +22,10 @@
                     </label>
                     <input type="hidden" name="{{csrf_key}}" value="{{csrf_token}}">
                   </div>
-                  <button type="submit" class="btn btn-default col-xs-6 col-xs-offset-3">LogIn</button>
+                  <button type="submit" class="btn btn-info col-xs-6 col-xs-offset-3">LOGIN</button>
                     <div class="form-group col-xs-12 text-center">
                       <span class="text-danger">{{errors.login}}</span>
-                        <br/><p>Don't have an Account? Sign up <a href="{{urlFor('register')}}">here</a> </p>
+                        <br/><p>Don't have an Account? Sign up <a href="{{urlFor('register')}}" style="color:#5bc0de;">here</a> </p>
                     </div>
 
                 </form>

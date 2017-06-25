6b18f13e07c4895614101d694874994586e13a46
tawanda nyakudjga <tawanda.nyakudjga@gmail.com>
2017-06-25 19:32:32 +0800

    move overview routes to controller

---
+++ app/HTTP/Controllers/Main/OverviewController.php
@@ -0,0 +1,32 @@
+<?php
+  namespace HTTP\Controllers\Main;
+  /**
+   * OverviewController
+   */
+  class OverviewController extends \HTTP\Controllers\BaseController
+  {
+    public function __invoke($req,$resp,$args)
+    {
+      $app = $this->container;
+      $year = isset($args['year'])?$args['year']:NULL;
+      if(!isset($year)){
+        $year = $year= date('Y');
+      }
+      $data = $app->Helper->getData($app,$app->auth->user_id,$year);
+      $overviewData = $app->Helper->yearOverView($app,$app->auth->user_id,$year);
+
+      $app->view->render($resp,'main/dashboard.php',[
+        'totalExp'=>$overviewData['totalExp'],
+        'totalInc'=>$overviewData['totalInc'],
+        'allIncomes'=>$overviewData['allIncomes'],
+        'allExpenses'=>$overviewData['allExpenses'],
+        'earned'=>$overviewData['earned'],
+        'spent'=>$overviewData['spent'],
+        'appData' => $data,
+        'page'    => 'overview',
+        'totals'  => []
+      ]);
+    }
+  }
+
+ ?>

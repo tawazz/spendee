7de575dac322a9744e74f31cc1b5b3f0b1327246
tawanda nyakudjga <tawanda.nyakudjga@gmail.com>
2017-08-16 20:16:46 +0800

    set daily task to run daily

--- tasks/dailyTasks.php
+++ tasks/dailyTasks.php
@@ -4,7 +4,7 @@
 
   $schedule = new Schedule();
   $schedule->run('php /app/app/bin/clearOldSessions.php')->daily()->description('clearOldSessions');
-  $schedule->run('php /app/app/bin/recurringExp.php')->everyFiveMinutes()->description('recurring expenses');
+  $schedule->run('php /app/app/bin/recurringExp.php')->daily()->description('recurring expenses');
 
   return $schedule;
  ?>

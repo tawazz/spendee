08cccb38a4a37414b11eb33d106bb8b7162b6b83
tawanda nyakudjga <tawanda.nyakudjga@gmail.com>
2017-08-20 08:26:07 +0800

    update clear cache command

--- app/bin/cache_clear.php
+++ app/bin/cache_clear.php
@@ -4,6 +4,7 @@
   use \HTTP\Services\ServiceProvider;
   use \Slim\App;
   use \Slim\Http\Environment;
+  use \Symfony\Component\Filesystem\Filesystem;
 
   $app = new App();
   $app->environment = Environment::mock([
@@ -14,6 +15,13 @@
 
   $cache = $container->cache;
   $cache->clear();
+  $filesystem = new Filesystem();
+  $views_cache ='/app/app/views/cache';
+
+  if ($filesystem->exists($views_cache)) {
+      $filesystem->remove($views_cache);
+      $container->log->debug('View Cache Cleared');
+  }
   $container->log->debug('Cache Cleared');
 
  ?>

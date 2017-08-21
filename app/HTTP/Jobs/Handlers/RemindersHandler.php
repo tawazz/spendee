de10f8e732b6d4eff56880d1c4bd86da675fc608
tawazz <tawanda.nyakudjga@gmail.com>
2017-08-21 02:29:42 +0000

    fix job being reserved all the time

--- app/HTTP/Jobs/Handlers/RemindersHandler.php
+++ app/HTTP/Jobs/Handlers/RemindersHandler.php
@@ -7,51 +7,56 @@
   class Remindershandler extends \HTTP\Jobs\Handlers\BaseHandler
   {
     public function fire($job, $data){
+      try{
+        $container = $this->container;
+        $pb = $container->pb;
+        $Carbon =$container->Carbon;
+        $User = $container->User;
 
-      $container = $this->container;
-      $pb = $container->pb;
-      $Carbon =$container->Carbon;
-      $User = $container->User;
+        $today = $Carbon->now()->hour(0)->minute(0)->second(0);
 
-      $today = $Carbon->now()->hour(0)->minute(0)->second(0);
+        $expenses = RecurringExpense::where('ended',false)->get();
+        $reminders = [];
+        foreach ($expenses as $exp) {
+          $expense = $exp->expense();
+          $container->auth = $User->get($expense->user_id);
+          $repeat = null;
 
-      $expenses = RecurringExpense::where('ended',false)->get();
-      $reminders = [];
-      foreach ($expenses as $exp) {
-        $expense = $exp->expense();
-        $container->auth = $User->get($expense->user_id);
-        $repeat = null;
-
-        switch ($exp->repeat) {
-          case '30':
-            $repeat = $Carbon->parse($expense->date)->addMonths(1 * $exp->interval);
-            array_push($reminders,$this->getReminder($exp,$repeat,$today));
-            break;
-          case '365':
-            $repeat = $Carbon->parse($expense->date)->addYears(1 * $exp->interval);
-            array_push($reminders,$this->getReminder($exp,$repeat,$today ));
-            break;
-          case '14':
-            $repeat = $Carbon->parse($expense->date)->addWeeks(2 * $exp->interval);
-            array_push($reminders,$this->getReminder($exp,$repeat,$today ));
-            break;
-          case '7':
-            $repeat = $Carbon->parse($expense->date)->addWeeks(1 * $exp->interval);
-            array_push($reminders,$this->getReminder($exp,$repeat,$today));
-            break;
-          case '1':
-            $repeat = $Carbon->parse($expense->date)->addDays(1 * $exp->interval);
-            array_push($reminders,$this->getReminder($exp,$repeat,$today));
-            break;
+          switch ($exp->repeat) {
+            case '30':
+              $repeat = $Carbon->parse($expense->date)->addMonths(1 * $exp->interval);
+              array_push($reminders,$this->getReminder($exp,$repeat,$today));
+              break;
+            case '365':
+              $repeat = $Carbon->parse($expense->date)->addYears(1 * $exp->interval);
+              array_push($reminders,$this->getReminder($exp,$repeat,$today ));
+              break;
+            case '14':
+              $repeat = $Carbon->parse($expense->date)->addWeeks(2 * $exp->interval);
+              array_push($reminders,$this->getReminder($exp,$repeat,$today ));
+              break;
+            case '7':
+              $repeat = $Carbon->parse($expense->date)->addWeeks(1 * $exp->interval);
+              array_push($reminders,$this->getReminder($exp,$repeat,$today));
+              break;
+            case '1':
+              $repeat = $Carbon->parse($expense->date)->addDays(1 * $exp->interval);
+              array_push($reminders,$this->getReminder($exp,$repeat,$today));
+              break;
+          }
         }
-      }
 
-      foreach ($reminders as $reminder) {
-        if (isset($reminder)) {
-          $pb->allDevices()->pushNote("Spendee - Recurring Expenses",$reminder);
+        foreach ($reminders as $reminder) {
+          if (isset($reminder)) {
+            $pb->allDevices()->pushNote("Spendee - Recurring Expenses",$reminder);
+          }
         }
+        #$container->log->info('Notifications sent');
+        echo $Carbon->now()->toDayDateTimeString()." Notifications Sent...\n";
+      } catch(\Exception $e) {
+        dump($e);
+        $job->release();
       }
-      $container->log->info('Notifications sent');
     }
 
     public function getReminder($exp,$repeat,$today)

6e35f753ed09e17ebcc1004b4285ff11df6d63d6
tawanda nyakudjga <tawanda.nyakudjga@gmail.com>
2017-08-26 07:54:14 +0800

    add console for debugging

---
+++ app/bin/shell.php
@@ -0,0 +1,19 @@
+<?php
+
+require_once __DIR__.'/../../vendor/autoload.php';
+
+use \HTTP\Services\ServiceProvider;
+use \Slim\App;
+use \Slim\Http\Environment;
+use \Symfony\Component\Filesystem\Filesystem;
+
+$app = new App();
+$app->environment = Environment::mock([
+    'REQUEST_URI' => '/'
+]);
+$container = $app->getContainer();
+$container->register(new ServiceProvider());
+
+
+
+ ?>

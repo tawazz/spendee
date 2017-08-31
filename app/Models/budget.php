45d62ea9857465ca6902f891cea70933162cb666
tawazz <tawanda.nyakudjga@gmail.com>
2017-08-31 15:18:39 +0000

    fix budget calculations

--- app/Models/budget.php
+++ app/Models/budget.php
@@ -31,7 +31,7 @@ class Budget extends Table
         foreach ($tags as $tagone) {
           foreach ($tagone as $tag) {
             $tagInfo = $app->Tags->find();
-            $amount = (int) $this->db->query("SELECT Sum(exp.cost ) as total FROM exp_tags as tag,expenses as exp where tag_id = ? and exp.exp_id = tag.exp_id and  exp.date >= ? and exp.date < ? and exp.user_id = ?",[$tag->id,$startDate,$endDate,$app->auth->user_id])->first()->total;
+            $amount = (int) $this->db->query("SELECT Sum(exp.cost ) as total FROM exp_tags as tag,expenses as exp where tag_id = ? and exp.exp_id = tag.exp_id and  exp.date >= ? and exp.date <= ? and exp.user_id = ?",[$tag->id,$startDate,$endDate,$app->auth->user_id])->first()->total;
             if($amount > 0){
               $exptags[$tag->name] =  $amount;
             }
@@ -41,10 +41,10 @@ class Budget extends Table
         $budget->tags = $exptags;
         $budget->spent = $spent;
         $budget->spentPercentage = number_format( ($spent/$budget->amount)*100,2,'.',',');
-        $budget->future = Carbon::now('Australia/Perth')->lt($budgetDate);
+        $budget->future = Carbon::now('Australia/Perth')->lt($budgetDate->copy()->addMonth());
         $budget->spendingLeft = ($budget->future == true ) ? (($budget->amount - $spent)/ (cal_days_in_month(CAL_GREGORIAN,$month, $year)) ): number_format( ($budget->amount - $spent)/(cal_days_in_month(CAL_GREGORIAN,$month, $year)-(int)date('j')),2,'.',',' );
         $budget->expired = Carbon::now('Australia/Perth')->gt($budgetDate->addMonth());
-        $budget->saved =  ($budget->amount - $spent > 0) ? number_format( ($budget->amount - $spent),2,'.',',' ) : number_format( ( $spent - $budget->amount ),2,'.',',' );
+        $budget->saved =  ($budget->amount - $spent > 0) ? $budget->amount - $spent : $spent - $budget->amount;
       }
 
       return $budgets;

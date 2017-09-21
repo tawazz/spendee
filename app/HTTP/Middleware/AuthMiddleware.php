829dcb48726a81bbb0b9d05722d393ce2d723ef8
tawazz <tawanda.nyakudjga@gmail.com>
2017-09-21 06:19:50 +0000

    return 401 on unauthorized api requests

--- app/HTTP/Middleware/AuthMiddleware.php
+++ app/HTTP/Middleware/AuthMiddleware.php
@@ -6,9 +6,13 @@ use GuzzleHttp\Exception\RequestException;
 *
 */
 class AuthMiddleware extends \HTTP\Middleware\BaseMiddleware {
-
+  protected $is_api = false;
   public function __invoke($req,$resp,$next){
+    $this->is_api = (strpos($req->getUri()->getPath(), 'api') !== false);
     $resp = $this->run($req,$resp);
+    if($resp->getStatusCode() > 400){
+      return $resp;
+    }
     $resp = $next($req,$resp);
     return $resp;
   }
@@ -23,6 +27,9 @@ class AuthMiddleware extends \HTTP\Middleware\BaseMiddleware {
             "auth"=>$user
         ]);
       } else {
+        if( $this->is_api) {
+          return $this->Cookie->deleteCookie($resp,'remember')->withStatus(401)->withJson('Login required to access the resource');
+        }
         $this->flash->addMessage('global','Login required to access the resource');
         $resp = $resp->withStatus(302)->withHeader('Location','/login');
       }
@@ -38,6 +45,7 @@ class AuthMiddleware extends \HTTP\Middleware\BaseMiddleware {
       $cookie = $this->Cookie->getCookie($req,'remember');
       $hash = $cookie->getValue();
       $exist = $this->Remember->find('first',['where'=>['hash','=',$hash]]);
+
       if(isset($exist))
       {
         $id = $exist->user_id;
@@ -50,17 +58,24 @@ class AuthMiddleware extends \HTTP\Middleware\BaseMiddleware {
             ]);
         }else {
           $this->User->removeRemember($user->user_id);
+          if( $this->is_api) {
+            return $resp->withStatus(401)->withJson('Login required to access the resource');
+          }
           $this->flash->addMessage('global','Login required to access the resource');
-          $resp = $this->Cookie->deleteCookie($resp,'remember');
-          $resp->withStatus(302)->withHeader('Location','/login');
+          $resp = $this->Cookie->deleteCookie($resp,'remember')->withStatus(302)->withHeader('Location','/login');
         }
       }else{
+        if( $this->is_api) {
+          return $resp->withStatus(401)->withJson('Login required to access the resource');
+        }
         $this->flash->addMessage('global','Login required to access the resource');
-        $resp = $this->Cookie->deleteCookie($resp,'remember');
-        $resp->withStatus(302)->withHeader('Location','/login');
+        $resp = $this->Cookie->deleteCookie($resp,'remember')->withStatus(302)->withHeader('Location','/login');
       }
     }
     else{
+      if( $this->is_api) {
+        return $resp->withStatus(401)->withJson('Login required to access the resource');
+      }
       $this->flash->addMessage('global','Login required to access the resource');
       $resp = $resp->withStatus(302)->withHeader('Location','/login');
 

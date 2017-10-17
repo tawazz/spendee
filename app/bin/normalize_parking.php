92a68d8ac00459fba2719f1bdf13bc7df3980a81
tawazz <tawanda.nyakudjga@gmail.com>
2017-10-17 14:15:18 +0000

    normalize imported expenses

--- app/bin/normalize_parking.php
+++
@@ -1,6 +0,0 @@
-<?php
-  require_once __DIR__.'/shell.php';
-
-  $query = "update expenses set name = 'parking' where LOWER(name) like '%parking%'";
-  dump(\Tazzy\Database\DB::connect()->query($query));
- ?>

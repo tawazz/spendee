6a7f1b7df8724706e1d2b78e978b500c05f78483
tawanda nyakudjga <tawanda.nyakudjga@gmail.com>
2017-10-28 10:04:04 +0800

    mobile api WIP

--- app/HTTP/Controllers/API/MobileApi.php
+++ app/HTTP/Controllers/API/MobileApi.php
@@ -40,54 +40,141 @@
             $year = isset($args['year']) ? $args['year'] : Null;
             $month = isset($args['month']) ? $args['month'] : Null;
             $day = isset($args['day']) ? $args['day'] : Null;
-            $data = $app->Budget->getBudgetData($app,$year,$month);
-            return $resp->withJson($data);
-            $cache = $app->cache;
-            $cache_key = 'api.budget.'.$app->auth->id.'.'.$year.'.'.$month;
-            $data = [];
-            if (!$cache->has($cache_key)) {
-              $data = $app->Budget->getBudgetData($app,$year,$month);
-              $cache->set($cache_key,$data);
-            } else {
-              $data = $cache->get($cache_key);
-            }
+            $user_id = isset($args['user']) ? $args['user'] : Null;
 
-            return $resp->withJson($data);
+            if(isset($year)&& isset($month) && isset($day) ){
+
+              $totalexp = $app->Exp->read($user_id)->totalExp($year."-".$month."-".$day,$year."-".$month."-".($day+1));
+              $totalinc = $app->Inc->read($user_id)->totalInc($year."-".$month."-".$day,$year."-".$month."-".($day+1));
+              $allExpenses = $app->Exp->read($user_id)->activity($year."-".$month."-".$day,$year."-".$month."-".($day+1));
+              $allIncomes= $app->Inc->read($user_id)->activity($year."-".$month."-".$day,$year."-".$month."-".($day+1));
+              $itemSpent = $app->Exp->read($user_id)->allActivity($year."-".$month."-".$day,$year."-".$month."-".($day+1));
+              $exptags = $app->ExpTags->tagData($user_id,false,0,$year."-".$month."-1",$year."-".$month."-".($day+1));
+          }else if(isset($year)&& isset($month) ){
+
+            $totalexp = $app->Exp->read($user_id)->totalExp($year."-".$month."-1",$year."-".($month+1)."-1");
+            $totalinc = $app->Inc->read($user_id)->totalInc($year."-".$month."-1",$year."-".($month+1)."-1");
+            $allExpenses = $app->Exp->read($user_id)->activity($year."-".$month."-1",$year."-".($month+1)."-1");
+            $itemSpent = $app->Exp->read($user_id)->allActivity($year."-".$month."-1",$year."-".($month+1)."-1");
+            $allIncomes = $app->Inc->read($user_id)->activity($year."-".$month."-1",$year."-".($month+1)."-1");
+            $exptags = $app->ExpTags->tagData($user_id,false,0,$year."-".$month."-1",$year."-".($month+1)."-1");
+
+          }else if(isset($year)){
+            $totalexp = $app->Exp->read($user_id)->totalExp($year."-"."1"."-1",($year+1)."-1-1");
+            $totalinc = $app->Inc->read($user_id)->totalInc($year."-"."1"."-1",($year+1)."-1-1");
+            $allExpenses = $app->Exp->read($user_id)->activity($year."-"."1"."-1",($year+1)."-1-1");
+            $allIncomes = $app->Inc->read($user_id)->activity($year."-"."1"."-1",($year+1)."-1-1");
+            $itemSpent= $app->Exp->read($user_id)->allActivity($year."-"."1"."-1",($year+1)."-1-1");
+            $exptags = $app->ExpTags->tagData($user_id,false,0,$year."-"."1"."-1",($year+1)."-1-1");
+
+          }else{
+              $month= date('m');
+              $year= date('Y');
+              $day = date('d');
+              $totalexp = $app->Exp->read($user_id)->totalExp($year."-".$month."-1",$year."-".($month+1)."-1");
+              $totalinc = $app->Inc->read($user_id)->totalInc($year."-".$month."-1",$year."-".($month+1)."-1");
+              $allExpenses = $app->Exp->read($user_id)->activity($year."-".$month."-1",$year."-".($month+1)."-1");
+              $allIncomes= $app->Inc->read($user_id)->activity($year."-".$month."-1",$year."-".($month+1)."-1");
+              $itemSpent = $app->Exp->read($user_id)->allActivity($year."-".$month."-1",$year."-".($month+1)."-1");
+              $exptags = $app->ExpTags->tagData($user_id,false,0,$year."-".$month."-1",$year."-".($month+1)."-1");
+
+          }
+
+          $totalinc = isset($totalinc->sum)?$totalinc->sum:0;
+          $totalexp = isset($totalexp->sum)?$totalexp->sum:0;
+
+          $expDates = [];
+          $expData = [];
+
+          $incDates = [];
+          $incTotals = [];
+
+          foreach ($allExpenses as $exp) {
+            $expDates[$exp->date] = NULL;
+          }
+
+          foreach ($allIncomes as $inc) {
+            $incDates[$inc->date] = NULL;
+          }
+
+          foreach ($expDates as $key => $value) {
+            $expenses = [];
+            foreach ($allExpenses as $exp) {
+              if($key == $exp->date){
+                array_push($expenses,$exp);
+                $expDates[$exp->date] = $expenses;
+              }
+            }
+          }
+
+          foreach ($incDates as $key => $value) {
+            $incomes = [];
+            foreach ($allIncomes as $inc) {
+              if($key == $inc->date){
+                array_push($incomes,$inc);
+                $incDates[$inc->date] = $incomes;
+              }
+            }
+          }
+
+
+          $response = [
+            "exp_total" => $totalexp,
+            "inc_total" => $totalinc,
+            "balance"   => $totalinc - $totalexp,
+            "exp_data"  => $expDates,
+            "inc_data"  => $incDates
+          ];
+
+          return $resp->withJson($response);
         }
 
-        public function create($req, $resp,$args)
+        public function addExpense($req,$resp,$args)
         {
           $app = $this->container;
           $body = json_decode($req->getBody()->getContents());
-          $data = [
-            "name"       => $body->name,
-            "amount"     => Utils::fixMoneyInput($body->amount),
-            "date" => $body->date,
-            "user_id"    => $app->auth->id
-          ];
-          $bud_id = $app->Budget->save($data);
+          $body->item->cost = Utils::fixMoneyInput($body->item->cost);
+          $exp_id = $app->Exp->save($body->item);
+          if(isset($body->tags)){
 
-          if(in_array(0,$body->tags)){
-            $tags = $app->Tags->find('all');
-            foreach ($tags as $tag) {
-              $data = [
-                "tag_id"     => $tag->id,
-                "bud_id"     => $bud_id
-              ];
-              $app->BudgetTag->save($data);
+            foreach ($body->tags as $tag) {
+                $tags_data = [
+                  'exp_id' => $exp_id,
+                  'tag_id' => $tag["tag_id"]
+                ];
+                $app->ExpTags->save($tags_data);
             }
+
           }
-          else
-          {
-            foreach ($body->tags as $tag_id) {
-              $data = [
-                "tag_id"     => $tag_id,
-                "bud_id"     => $bud_id
-              ];
-              $app->BudgetTag->save($data);
+
+          $response = [
+            "added" => true
+          ];
+          return $resp->withJson($response);
+        }
+        
+        public function addIncome($req,$resp,$args)
+        {
+          $app = $this->container;
+          $body = json_decode($req->getBody()->getContents());
+          $body->item->cost = Utils::fixMoneyInput($body->item->cost);
+          $id = $app->Inc->save($body->item);
+          if(isset($body->tags)){
+
+            foreach ($body->tags as $tag) {
+                $tags_data = [
+                  'inc_id' => $id,
+                  'tag_id' => $tag["tag_id"]
+                ];
+                $app->IncTags->save($tags_data);
             }
+
           }
-          return $resp->withJson($app->Budget->get($bud_id));
+
+          $response = [
+            "added" => true
+          ];
+          return $resp->withJson($response);
         }
         public function update($req, $resp,$args)
         {
--- app/routes/mobile/routes.php
+++ app/routes/mobile/routes.php
@@ -1,11 +1,11 @@
 <?php
 use \HTTP\Controllers\API\MobileApi;
 $this->post('/auth',MobileApi::class.':auth');
-$this->get('/data/{user}[/{year}[/{month}[/{day}]]]',MobileApi::class.'');
-$this->post('/expenses/add',MobileApi::class.'');
+$this->get('/data/{user}[/{year}[/{month}[/{day}]]]',MobileApi::class.':get');
+$this->post('/expenses/add',MobileApi::class.':addExpense');
 $this->post('/expenses/update',MobileApi::class.'');
 $this->post('/expenses/delete',MobileApi::class.'');
-$this->post('/incomes/add',MobileApi::class.'');
+$this->post('/incomes/add',MobileApi::class.':addIncome');
 $this->post('/incomes/update',MobileApi::class.'');
 $this->post('/incomes/delete',MobileApi::class.'');
 $this->get('/graph/{type}/{user}[/{year}]',MobileApi::class.'');

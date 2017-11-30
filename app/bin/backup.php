b0e2aeeda736e3666930f6605581d840d2d1dc90
Tawanda Nyakudjga <tawanda.nyakudjga@agworld.com.au>
2017-11-30 09:21:04 +0800

    add logs and push notifications

--- app/bin/backup.php
+++ app/bin/backup.php
@@ -9,22 +9,29 @@ use BackupManager\Compressors;
 use BackupManager\Manager;
 use BackupManager\Filesystems\Destination;
 use BackupManager\Tasks;
+$pb = $container->pb;
 // build providers
-echo "backing up ...\n";
-$filesystems = new Filesystems\FilesystemProvider(new Config($container->Config->get('storage')));
-$filesystems->add(new HTTP\Helpers\AWSFS);
-$filesystems->add(new Filesystems\LocalFilesystem);
-$databases = new Databases\DatabaseProvider(new Config(['dev' => $container->Config->get('db')]));
-$databases->add(new Databases\MysqlDatabase);
-$compressors = new Compressors\CompressorProvider;
-$compressors->add(new Compressors\GzipCompressor);
-$compressors->add(new Compressors\NullCompressor);
-$dt = $container->Carbon->now();
-// build manager
-$manager =  new Manager($filesystems, $databases, $compressors);
-$manager->makeBackup()->run('dev', [
-  new Destination('local', 'backup.sql'),
-  new Destination('s3', "database/spendee_db_{$dt->toDateTimeString()}.sql")
-], 'gzip');
-unlink('/backup.sql.gz')
+try {
+  echo "backing up ...\n";
+  $filesystems = new Filesystems\FilesystemProvider(new Config($container->Config->get('storage')));
+  $filesystems->add(new HTTP\Helpers\AWSFS);
+  $filesystems->add(new Filesystems\LocalFilesystem);
+  $databases = new Databases\DatabaseProvider(new Config(['dev' => $container->Config->get('db')]));
+  $databases->add(new Databases\MysqlDatabase);
+  $compressors = new Compressors\CompressorProvider;
+  $compressors->add(new Compressors\GzipCompressor);
+  $compressors->add(new Compressors\NullCompressor);
+  $dt = $container->Carbon->now();
+  // build manager
+  $manager =  new Manager($filesystems, $databases, $compressors);
+  $manager->makeBackup()->run('dev', [
+    new Destination('local', 'backup.sql'),
+    new Destination('s3', "database/spendee_prod_{$dt->toDateTimeString()}.sql")
+  ], 'gzip');
+  unlink('/backup.sql.gz');
+  $pb->allDevices()->pushNote("Spendee - backup completed","");
+} catch (\Exception $e) {
+  $container->log->error($e->getMessage(),$e->getTrace());
+}
+
  ?>

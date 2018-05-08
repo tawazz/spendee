8f3993e67f1bd5e91521da0884f609cba5c3ee0f
Tawanda Nyakudjga <tawanda.nyakudjga@agworld.com.au>
2018-05-08 10:50:12 +0800

    change file ownership to www-data for application files

--- boot.sh
+++ boot.sh
@@ -4,4 +4,5 @@ php vendor/bin/phinx migrate -c app/config/config-phinix.php
 cd public
 npm install -g yarn
 yarn run build
+cd /app && chown -R www-data:www-data .
 exec /usr/bin/supervisord -n "$@"
